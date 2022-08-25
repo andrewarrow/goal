7f530c2cce6bddedb60b82043c7096a46d691750
Andrew Arrow <oneone@gmail.com>
2022-08-25 13:37:04 -0700

    testing failing for view1 width height

--- layout/view_test.go
+++ layout/view_test.go
@@ -17,7 +17,7 @@ func TestLayoutSetup(t *testing.T) {
 	}
 }
 
-func TestLayoutN(t *testing.T) {
+func TestLayoutRenders(t *testing.T) {
 	LoadFromFile("../examples/layout1.json")
 	setupRootAndBoard(30, 60)
 	processSubviewsForIdMap(&root.Root, root.Root.Subviews)
@@ -26,5 +26,26 @@ func TestLayoutN(t *testing.T) {
 	for _, viewName := range views {
 		view := idMap[viewName]
 		fmt.Println(viewName, view.renderedView.String())
+	}
+	rootTop := idMap["root"].renderedView.Top == 0
+	rootLeading := idMap["root"].renderedView.Leading == 0
+	if !rootTop || !rootLeading {
+		t.Errorf("root Top or Leading != 0")
+	}
+	rootWidth := idMap["root"].renderedView.Width == 60
+	rootHeight := idMap["root"].renderedView.Height == 30
+	if !rootWidth || !rootHeight {
+		t.Errorf("root Width or Height wrong")
+	}
+
+	view1Top := idMap["view1"].renderedView.Top == 2
+	view1Leading := idMap["view1"].renderedView.Leading == 3
+	if !view1Top || !view1Leading {
+		t.Errorf("view1 Top or Leading wrong")
+	}
+	view1Width := idMap["view1"].renderedView.Width == 60-6
+	view1Height := idMap["view1"].renderedView.Height == 30-4
+	if !view1Width || !view1Height {
+		t.Errorf("view1 Width or Height wrong")
 	}
 }
